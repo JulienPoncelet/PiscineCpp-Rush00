e7bfc5a22d036b583f8d3f7c44a109c19efc0045
kalenedral <vivian.gedon@gmail.com>
2015-01-11 11:13:33 +0100

    Window - link to map to disply her + sample test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,16 +3,20 @@
 int					main(void) {
 	AObject			* player = new Player();
 	CObject			* list   = new CObject(player);
-	Map				* map    = new Map(20, 40, list);
+	Map				* map    = new Map(60, 30, list);
 
-	player->move(9, 19);
+	Window			win (map);
+
 	player->setMap(map);
+	player->move(5, 29);
 
-	AObject 		* enemy = new Enemy(0, 19, MIDDLEGROUND, map, 1, 1, 1);
+	AObject 		* enemy = new Enemy(5, 0, MIDDLEGROUND, map, 1, 1, 1);
 	map->pushObject(enemy);
 
-	std::cout << *list << std::endl;
-	std::cout << *map << std::endl;
+	// std::cout << *list << std::endl;
+	// std::cout << *map << std::endl;
+
+	win.menu();
 
 	return 0;
 }
