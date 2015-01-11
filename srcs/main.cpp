80142d42a86a79bbfc22f84629cd7471fec09722
Julien-jacques PONCELET <jponcele@e2r7p4.42.fr>
2015-01-11 09:58:43 +0100

    main test

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,7 +1,18 @@
 # include <ft_retro.hpp>
 
 int					main(void) {
-	// AObject			player = Player(9, 39, );
+	AObject			* player = new Player();
+	CObject			* list   = new CObject(player);
+	Map				* map    = new Map(20, 40, list);
+
+	player->move(9, 19);
+	player->setMap(map);
+
+	AObject 		* enemy = new Enemy(0, 19, MIDDLEGROUND, map, 1, 1, 1);
+	map->pushObject(enemy);
+
+	std::cout << *list << std::endl;
+	std::cout << *map << std::endl;
 
 	return 0;
 }
